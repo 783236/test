8160f5bb9cc3a2c6e1c0c674d089604efe49f3e9
783236 <54897132+783236@users.noreply.github.com>
2019-10-04 11:00:44 -0400

    assignment start

--- Pages/Assign.js
+++ Pages/Assign.js
@@ -1,11 +1,20 @@
 function greeting(){
 	var box=prompt("What's your name");
 	alert("Hello "+box);
+	alert("Our first test was to input user information");
 }
 
 function second(){
-	var box=prompt("Go to second?");
-	
+	var box=prompt("What is the last degree of the triangle?""60 and 60");
+	if (user_interger "90") {
+		alert("Correct!");
+	}
+	else {
+		alert("Wrong! Try Again.");
+	}
+ 
+
+
 }
 
 function submit(){
