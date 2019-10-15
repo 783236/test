e9b94dc8cbe4a0154a729b2cd365f16443ec5ff6
783236 <54897132+783236@users.noreply.github.com>
2019-10-15 17:33:25 -0400

    Update Assign.js

--- Pages/Assign.js
+++ Pages/Assign.js
@@ -13,8 +13,6 @@ function triangle(){
 	}
 
 
-
-
 function number(){
 	
 	var text = alert("Numbers");
@@ -36,63 +34,20 @@ function number(){
 		}
 		i++;
 	}
-	
-
 }
-
-
-
-
-
-
 
 
 function century(){
-	var century1to1000 = parseInt(prompt("State a year"));
-	if ("century1to1000" < 100) {
-		return "1st Century";
-	} else if ("centuryto1000" > 100 < 200) {
-		alert("2nd Century");
-	} else if ("centuryto1000"  > 200 < 300) {
-		alert("3rd Century");
-	} else if ("century1to1000" >300 <400) {
-		alert("4th Century");
-	} if ("century1to1000" >400 <500) {
-		alert("5th Century");
-	} if ("century1to1000" >500 <600) {
-		alert("6th Century");
-	} if ("century1to1000" >600 <700) {
-		alert("7th Century");
-	} if ("century1to1000" >700 <800) {
-		alert("8th Century");
-	} if ("century1to1000" >800 <900) {
-		alert("9th Century");
-	} if ("century1to1000" >900 <1000) {
-		alert("10th Century");
-	}
-	var centuryto2000 = parseInt(prompt("State a year"));
-	if ("century1to2000" >1000 <1100) {
-		alert("11th Century");
-	} if ("century1to2000" >1100 <1200) {
-		alert("12th Century");
-	} if ("century1to2000" >1200 <1300) {
-		alert("13th Century");
-	} if ("century1to2000" >1300 <1400) {
-		alert("14th Century");
-	} if ("century1to2000" >1400 <1500) {
-		alert("15th Century");
-	} if ("century1to2000" >1500 <1600) {
-		alert("16th Century");
-	} if ("century1to2000" >1600 <1700) {
-		alert("17th Century");
-	} if ("century1to2000" >1700 <1800) {
-		alert("18th Century");
-	} if ("century1to2000" >1800 <1900) {
-		alert("19th Century");
-	} if ("century1to2000" >1900 <2000) {
-		alert("20th Century");
-	} if ("century1to2000" >2000 <2100) {
-		alert("21st Century");
-	}
-
+	var text
+	var century1000 ("input");
+	switch (century1000) {
+		case > 100 :
+			text = "1st Century";
+		break;
+		case < 100, > 200 :
+			text = "2nd Century";
+		break;
 }
+
+
+ 
